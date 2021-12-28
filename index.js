00319389141f1324999aa3d71b99889c2147b9ee
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-12-28 16:40:22 +0100

    FEAT: add fourSection

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+
+/* SUPER SIMPLE SLIDER */
 jQuery(function ($) {
     $('.slider').sss();
 });
 
+/*
+ANIMATION WOW */
 new WOW().init();
 
 
+/* SLIDER GLIDER */
+window.addEventListener('load', function() {
+    new Glider(document.querySelector('.fourSection__list'), {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        draggable: true,
+        responsive: [
+            {
+                // screens greater than >= 775px
+                breakpoint: 768,
+                settings: {
+                    // Set to `auto` and provide item width to adjust to viewport
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                },
+            },
+            {
+                // screens greater than >= 1024px
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                },
+            },
+        ],
+    })
+})
